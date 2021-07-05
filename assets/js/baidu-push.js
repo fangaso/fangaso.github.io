7a40fd03f9e790121ed119f96f3c16c3093b778f
fangaso <61532453+fangaso@users.noreply.github.com>
2021-07-06 00:28:00 +0800

    update baidu-push.js

---
+++ assets/js/baidu-push.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?07051167d391f8f859d814131d9a120d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
